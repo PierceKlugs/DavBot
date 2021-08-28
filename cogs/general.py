d1db007d6921856d0d47a53c4969d75df8cf5661
PierceKlugs <55001667+PierceKlugs@users.noreply.github.com>
2021-08-28 18:26:20 -0500

    Added Polling Touch Up

--- cogs/general.py
+++ cogs/general.py
@@ -28,16 +28,18 @@ class General(commands.Cog):
         await ctx.author.send(f"Canvas: https://fhu.instructure.com/login/canvas \nGoogle Classroom: https://classroom.google.com/u/0/h \nGitHub: https://github.com/ \nVisual Studio Code: https://code.visualstudio.com/ \nPython: https://www.python.org/downloads/")
 
     @commands.command()
-    async def stuff(self, ctx: commands.Context):
+    async def helps(self, ctx: commands.Context):
         await ctx.author.send(f"```yaml\nDavBot | Commands\n=================================\n$assist - Notifies Moderators and Owners you need assistance.\n\n$links - Dav sends you a message with all class links.\n\n$legendary <NAME OF PROFESSOR> - Displays a GIF of a Professor (Options: casey,scott,nichols)\n\n$SI <OPTION> - Will do SI commands (Options: schedule, assist)\n\n```")
 
     @commands.command()
     async def poll(self, ctx: commands.Context, *question):
-        poll_item = Embed(title = f"{' '.join(question)}", description = "Respond down below:", colour=Color.from_rgb(128, 0, 0))
+        poll_item = Embed(title = f"{' '.join(question)}", description = "DavBot Polls!\nRespond to the poll by pressing the thumbs up or down!", colour=Color.from_rgb(255, 61, 61))
         poll_item.set_thumbnail(url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwaterview.org%2Fwp-content%2Fuploads%2F2018%2F12%2Fdavid-shannon.jpg&f=1&nofb=1")
+        poll_item.set_footer(text=f"Polled by {ctx.author.name}")
         sent_message = await ctx.send(embed=poll_item)
-        await sent_message.add_reaction("🇽")
-        await sent_message.add_reaction("✅")
+        await sent_message.add_reaction("👍")
+        await sent_message.add_reaction("👎")
+        
         #poll_embed = discord.Embed(title = f"{question}", description = "Respond down below:")
         #sent_message = await ctx.send(embed = poll_embed)
         #await sent_message.add_reaction("🇽")
