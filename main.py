83adb62b803064189bf7790c0931e9e59c8ec7d6
Cameron Pierce <55001667+PierceKlugs@users.noreply.github.com>
2021-11-01 18:59:56 -0500

    Removed Events

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from cogs import *
 import os
 from discord.ext import commands
 from discord.ext.commands.core import Command
+import requests
+import time
 
 #Important Variables
 
@@ -13,13 +15,39 @@ bot.remove_command('help')
 #Load all Cogs
 bot.load_extension("cogs.si")
 bot.load_extension("cogs.general")
-bot.load_extension("cogs.events")
+
 
 #Start Up
 @bot.event
 async def on_ready():
     print(f"{bot.user} is online!")
-    
+    channel = bot.get_channel(892118314933948456)
+    weatherurl = requests.get(
+    f"https://api.weather.gov/points/35.4393,-88.6418").json()
+        #channel = self.bot.get_channel(892118314933948456)
+    forecast = requests.get(weatherurl["properties"]["forecast"]).json()["properties"]
+    while True:
+        # channel.send(f"{weatherurl.json()}")
+        times = forecast["periods"]
+        message = ""
+
+        # Temperature Info
+        period = None
+        temperature = 0
+        windspeed = 0
+        shortforecast = ""
+        detailedforecast = ""
+
+        # Print
+        for i in times:
+            period = i["name"]
+            temperature = i["temperature"]
+            windspeed = i["windSpeed"]
+            shortforecast = i["shortForecast"]
+            detailedforecast = i["detailedForecast"]
+            message = f"```WEATHER\n============================\nTime: {period}\nTemperature: {temperature}\nWindspeed: {windspeed}\n```"
+            channel.send(f"{message}")
+        time.sleep(86400)
 
 
 #Token and Run
