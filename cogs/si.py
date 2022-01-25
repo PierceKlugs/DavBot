fa90c01047a77b0098fa935e3225f285646ee21b
PierceKlugs <55001667+PierceKlugs@users.noreply.github.com>
2022-01-25 16:28:55 -0600

    Removed Weather API Added Updated Variables and SI Schedule

--- cogs/si.py
+++ cogs/si.py
@@ -10,32 +10,36 @@ class SI(commands.Cog):
     @commands.command()
     #The SI command is a command designated as a tool for an SI in Computer Science
     async def SI(self, ctx: commands.Context, arg1: str):
-        
+        si_channels = [933846939735293962]
+        si_id = 131420748823789568
+
         #Prints out the SI Schedule
         #WARNING: Change the schedule based off the school year
         if (arg1 == "schedule"):
-            cis171_01 = self.bot.get_channel(877927229382881290)
-            cis171_02 = self.bot.get_channel(877963775746134106)
-            if(ctx.author.id == 131420748823789568):
-                await cis171_01.send("```\nOffice Hours:\nTuesday: 7:00pm - 8:00pm\nWednesday: 8:00pm - 9:00pm\n\nSI Sessions:\nMonday/Thursday: 7:00pm - 8:00pm\n```")
-                await cis171_02.send("```\nOffice Hours:\nTuesday: 7:00pm - 8:00pm\nWednesday: 8:00pm - 9:00pm\n\nSI Sessions:\nMonday/Thursday: 7:00pm - 8:00pm\n```")
+            #Schedule File
+            schedule_file = open("./schedule/schedule.txt")
+            schedule = schedule.readlines()[0]
+
+            #Send out Schedule
+            if(ctx.author.id == si_id):
+                for si_channel in si_channels:
+                    await self.bot.get_channel(si_channel).send(schedule)
             else:
-                await ctx.author.send("```\nOffice Hours:\nTuesday: 7:00pm - 8:00pm\nWednesday: 8:00pm - 9:00pm\n\nSI Sessions:\nMonday/Thursday: 7:00pm - 8:00pm\n```")
+                await ctx.author.send(schedule)
             
         
         #Alerts the SI that a user needs assistance
         #WARNING: Change the IDs according to the school year
-        elif (arg1 == "assist" and ctx.channel.id == 877927229382881290 or ctx.channel.id == 877963775746134106):
+        elif (arg1 == "assist" and ctx.channel.id in si_channel):
             assist = f"<@{ctx.author.id}>"
             await ctx.send(f"<@131420748823789568>, {assist} is in need of assistance.")
         
         #Alerts all students that the SI session has been cancelled, for CIS172
         #WARNING: Change the IDs according to the school year
-        elif (arg1 == "cancel" and ctx.author.id == 131420748823789568):
-            cis171_01 = self.bot.get_channel(877927229382881290)
-            cis171_02 = self.bot.get_channel(877963775746134106)
-            await cis171_01.send(f"<@877253774056501309>, <@131420748823789568> has cancelled the SI meeting for today...")
-            await cis171_02.send(f"<@877254044907876373>, <@131420748823789568> has cancelled the SI meeting for today...")
+        elif (arg1 == "cancel" and ctx.author.id == si_id):
+            for si_channel in si_channels:
+                    await self.bot.get_channel(si_channel).send(f"<@806916153255133184>, <@{si_id}> has cancelled the SI meeting for today...")
+
 
 
 def setup(bot: commands.Bot):
--- main.py
+++ main.py
@@ -23,42 +23,6 @@ async def on_ready():
     print(f"{bot.user} is online!")
     channel = bot.get_channel(782127136655671316)
 
-    while True:
-        weatherurl = requests.get(f"https://api.weather.gov/points/35.4393,-88.6418").json()
-        forecast = requests.get(weatherurl["properties"]["forecast"]).json()["properties"]
-        # channel.send(f"{weatherurl.json()}")
-        times = forecast["periods"]
-        message = ""
-
-        # Temperature Info
-        period = None
-        temperature = 0
-        windspeed = 0
-        shortforecast = ""
-        detailedforecast = ""
-        message = f"``` WEATHER\n============================\n"
-        list_periods = []
-        # Print
-        for i in times:
-            if i["number"] == 1 or i["number"] == 2 or i["number"] == 3:
-                period = i["name"]
-                temperature = i["temperature"]
-                windspeed = i["windSpeed"]
-                shortforecast = i["shortForecast"]
-                detailedforecast = i["detailedForecast"]
-                if i["number"] == 3:
-                    list_periods.append(f"Time: {period}\nTemperature: {temperature}F\nForecast: {shortforecast}```")
-                else:
-                    list_periods.append(f"Time: {period}\nTemperature: {temperature}F\nForecast: {shortforecast}\n\n")
-                    
-
-        for i in list_periods:
-            message += i
-                
-        
-        await channel.send(f"{message}")
-        time.sleep(86400)
-
 
 #Token and Run
 token = open("./Token/token.txt").readlines()[0]
